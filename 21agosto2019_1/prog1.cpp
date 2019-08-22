a74264dd590b1757ba6c4d3d0e93661431d09d3f
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2019-08-21 19:37:21 -0500

    basico #3, arreglos de apuntadores a funciones.

--- 21agosto2019_1/prog1.cpp
+++ 21agosto2019_1/prog1.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int muestra_mensaje(int a) {
-    return (a + 1);
+int sumar(int num1, int num2) {
+    return (num1 + num2);
+}
+
+int restar(int num1, int num2) {
+    return (num1 - num2);
 }
 
 int main() {
-    int(*p)(int) = NULL; //Declarar el apuntador, de tipo apuntador a funcion
-    p = &muestra_mensaje; //Asignar la direccion de la funcion al apuntador
-    int b = 0;
+    int(*p[2])(int,int) = {&sumar,&restar}; //Declarar el apuntador, de tipo apuntador a funcion    
+    int r = 0;
+    int opcion = 1; //0-sumar,1-restar
 
-    b = (*p)(50); //Invocar la funcion, por medio del apuntador
-    cout<<b<<endl;
+    //Avanced Programmer!
+    //r = (*p[opcion])(50,20); //Invocar la funcion, por medio del apuntador    
+
+    //Junior-normalito programmer
+    cout<<"Es q soy un junior!"<<endl;
+    if(opcion == 0) { //cuando vamos a sumar
+        r = sumar(50,20);
+    } else if(opcion == 1) { //cuando vamos a restar
+        r = restar(50,20);
+    }
+    cout<<r<<endl;
 
     return 0;
 }
