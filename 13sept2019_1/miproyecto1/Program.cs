7a5dc95360687e89cce05c8f0216354564df3742
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2019-09-14 12:31:55 -0500

    dotnet core. Solucion multiproyecto, con prooyecto que referencia otro proyecto.

---
+++ 13sept2019_1/miproyecto1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using miproyecto2;
+
+namespace miproyecto1
+{
+    public class Program
+    {        
+        public static void Main(string[] args)
+        {
+            int r = 0;
+            r = Operacion.sumar(100, 200);
+            Console.WriteLine("Resultado sumar: " + r);
+
+            r = Operacion.restar(200, 100);
+            Console.WriteLine("Resultado restar: " + r);
+        }
+    }
+}
