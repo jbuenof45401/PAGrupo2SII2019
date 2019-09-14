e82ee2530d046be5c3bd624af2bd15f6f787342c
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2019-09-14 12:06:59 -0500

    dotnet core, proyecto standalone.

---
+++ 11sept2019_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11sept2019_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, este es un proyecto standalone!.");
+        }
+    }
+}
