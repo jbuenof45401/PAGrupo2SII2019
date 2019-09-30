627d79db8af9ac39b36775540e05ab783d459839
Oscar Hernando Nivia Aragón <ONivia@agoracsc.com>
2019-09-29 19:35:18 -0500

    Herencia, su mecanica.

---
+++ 27sept2019_1/CalseB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _27sept2019_1
+{
+    public class ClaseB : ClaseA
+    {
+        public string nombre { get; set; }
+
+        public void m2() {
+            Console.WriteLine("m2 desde ClaseB");
+        }
+
+        public ClaseB() {
+        }
+    }
+}
---
+++ 27sept2019_1/ClaseA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _27sept2019_1
+{
+    public class ClaseA
+    {
+        public int nit { get; set; }
+
+        public void m1() {
+            Console.WriteLine("m1 desde ClaseA");
+        }
+
+        public ClaseA() {
+            //inicializar variables
+            //conexion a la DB
+        }
+    }
+}
